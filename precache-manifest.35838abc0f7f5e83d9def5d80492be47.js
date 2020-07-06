4b62484de0cd93e199accecba5d03ca803127579
Dell <nafishasnain0@gmail.com>
2020-07-06 11:40:48 -0400

    Updates

---
+++ precache-manifest.35838abc0f7f5e83d9def5d80492be47.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0275007192af2aa023ef722ebb427867",
+    "url": "/speed-typing-game/index.html"
+  },
+  {
+    "revision": "d186e4fc5330821ff587",
+    "url": "/speed-typing-game/static/js/2.628ca6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speed-typing-game/static/js/2.628ca6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ef574b62ba159b4b4b3",
+    "url": "/speed-typing-game/static/js/main.9c9f93f0.chunk.js"
+  },
+  {
+    "revision": "39fc1ce94c035919348f",
+    "url": "/speed-typing-game/static/js/runtime-main.e94936cb.js"
+  }
+]);
